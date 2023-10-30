9b541cb7b336e6779b9d199cfa8dc8ac770d2175
Denison Kolling <denisonkolling@gmail.com>
2023-10-30 09:05:47 -0300

    feat: implements axios and custom hook

--- src/shared/pages/Setup.jsx
+++ src/shared/pages/Setup.jsx
@@ -2,9 +2,11 @@ import { Box, Button, Typography } from '@mui/material';
 import React from 'react';
 import SelectField from '../components/SelectField';
 import TextFieldComp from '../components/TextFieldComp';
+import useAxios from '../hooks/useAxios';
 
 const Setup = () => {
-
+  const { response, error, loading } = useAxios({ url: "/api_category.php"})
+  console.log(response)
   const handleSubmit = (e) => {
     e.preventDefault();
   }
