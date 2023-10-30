a3c188a4dcd889336737b6a2567a7d9283af5896
Denison Kolling <denisonkolling@gmail.com>
2023-10-30 11:27:46 -0300

    feat: add redux

---
+++ src/shared/redux/reducer.js
@@ -0,0 +1,24 @@
+const initialState = {
+	question_category: '',
+	question_dificulty: '',
+	question_type: '',
+	amount_of_question: 50,
+	score: 0,
+};
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case CHANGE_CATEGORY:
+			return { ...state, question_category: action.payload };
+		case CHANGE_DIFFICULTY:
+			return { ...state, question_dificulty: action.payload };
+		case CHANGE_TYPE:
+			return { ...state, question_type: action.payload };
+		case CHANGE_AMOUNT:
+			return { ...state, amount_of_question: action.payload };
+		case CHANGE_SCORE:
+			return { ...state, score: action.payload };
+	}
+};
+
+export default reducer;
